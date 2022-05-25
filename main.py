23a4420f37f43db9ad2d41b141fef826c064e687
dmitrav <dmitrav@inbox.ru>
2022-05-25 18:38:35 +0200

    baseline model updated with normalization methods

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os.path
 
 import pandas, seaborn, random, time, numpy, seaborn, itertools
 from matplotlib import pyplot
@@ -12,7 +13,7 @@ from matplotlib import pyplot
 from constants import dilutions, aas, pps, mz_dict
 from constants import seed, split_ratio
 from constants import save_to, n_jobs
-from constants import normalized_pp_metabolites, normalized_pp_outliers
+from constants import normalized_pp_metabolites, normalized_aa_metabolites
 
 
 def get_data(path, sample_codes, metabolites=None):
@@ -123,10 +124,11 @@ def get_compounds_classes(features):
 
 
 def train_baseline_model(X, Y,
-                         outlier_thresholds=(0.4e6, 1.1e6, 1e7),  # -> [best MSE, tradeoff, best R2]
+                         outlier_thresholds=(2e6, 1e7),  # (best scores, all data points)
                          split_ratio=split_ratio,
                          random_seed=seed,
-                         save_to=save_to):
+                         save_to=save_to,
+                         plot_id=''):
 
     for t in outlier_thresholds:
 
@@ -139,11 +141,12 @@ def train_baseline_model(X, Y,
         #  - implement systematic hyperparameter search
         pipeline = Pipeline([
             ('scaler', StandardScaler()),
-            ('regressor', SVR(kernel='rbf'))
+            ('regressor', SVR())
         ])
 
         param_grid = {
-            'regressor__C': [50, 100, 150],
+            'regressor__C': [0.01, 0.1, 1, 10, 100, 1000],
+            'regressor__kernel': ['linear', 'rbf', 'sigmoid'],
             'regressor__epsilon': [0, 0.0001, 0.001, 0.01, 0.1]
         }
 
@@ -160,6 +163,7 @@ def train_baseline_model(X, Y,
         predictions = numpy.exp(reg.predict(X_test))
         r2 = r2_score(y_test, predictions)
         mse = mean_squared_error(numpy.log(y_test), numpy.log(predictions))
+        print('r2 = {:.3f}, mse = {:.3f}'.format(r2, mse))
 
         data = pandas.DataFrame({'true': y_test.values.reshape(-1), 'predicted': predictions,
                                  'dilution': X_test['dilution'].values.reshape(-1),
@@ -171,7 +175,10 @@ def train_baseline_model(X, Y,
         pyplot.title('R2 = {:.3f}, MSE(log) = {:.3f}, values < {:.1e}'.format(r2, mse, t))
         pyplot.grid()
         pyplot.tight_layout()
-        pyplot.savefig(save_to + 'regression_t={:.1e}.pdf'.format(t))
+
+        if not os.path.exists(save_to):
+            os.makedirs(save_to)
+        pyplot.savefig(save_to + 'regression_t={:.1e}{}.pdf'.format(t, plot_id))
         pyplot.close()
     # pyplot.show()
 
@@ -211,7 +218,7 @@ def train_with_missing_dilutions(X, Y, threshold=1e7, save_to=save_to):
             }
 
             scoring = {"r2": make_scorer(r2_score), "mse": make_scorer(mean_squared_error, greater_is_better=False)}
-            reg = GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring=scoring, refit='r2', cv=3, n_jobs=n_jobs)
+            reg = GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring=scoring, refit='r2', cv=5, n_jobs=n_jobs)
 
             start = time.time()
             reg.fit(X_train, numpy.log(y_train).values.ravel())
@@ -330,29 +337,48 @@ def train_with_missing_metabolites(X, Y, metabolite_group='aas', threshold=1e7, 
 
 if __name__ == '__main__':
 
-    # path = '/Users/andreidm/ETH/projects/normalization/res/SRM_v7/6b8943e1/normalized_6b8943e1.csv'
-    path = '/Users/andreidm/ETH/projects/calibration/data/SRM_SPP_normalized_2b632f6b.csv'
-    normalized_pp = get_data(path, ['P1_PP', 'P2_SPP', 'P2_SRM'])
-    normalized_aa = get_data(path, ['P1_AA', 'P2_SAA', 'P2_SRM'])
-
+    # INITIAL DATA
     path = '/Users/andreidm/ETH/projects/calibration/data/filtered_data.csv'
-    initial_pp = get_data(path, ['P1_PP', 'P2_SPP', 'P2_SRM'])
-    initial_aa = get_data(path, ['P1_AA', 'P2_SAA', 'P2_SRM'])
+    initial_pp = get_data(path, ['P1_PP', 'P2_SPP', 'P2_SRM'], metabolites=pps)
+    initial_aa = get_data(path, ['P1_AA', 'P2_SAA', 'P2_SRM'], metabolites=aas)
+    X, Y = assemble_dataset(initial_pp, initial_aa)
+    print('training for the initial data')
+    train_baseline_model(X, Y, plot_id='_initial', save_to=save_to+'baseline/')  # train and test on the entire dataset
 
-    plot_dilutions(initial_pp)
-    plot_dilutions(initial_aa)
+    # RALPS
+    path = '/Users/andreidm/ETH/projects/calibration/data/SRM_SPP_normalized_2b632f6b.csv'
+    normalized_pp = get_data(path, ['P1_PP', 'P2_SPP', 'P2_SRM'], metabolites=pps)
+    normalized_aa = get_data(path, ['P1_AA', 'P2_SAA', 'P2_SRM'], metabolites=aas)
+    X, Y = assemble_dataset(normalized_pp, normalized_aa)
+    print('training for the normalized data')
+    train_baseline_model(X, Y, plot_id='_ralps', save_to=save_to+'baseline/')  # train and test on the entire dataset
 
-    # X, Y = assemble_dataset(normalized_pp, normalized_aa)
-    # train_baseline_model(X,Y)  # train and test on the entire dataset
-    # train_with_missing_dilutions(X,Y, threshold=1.1e6)  # train to predict intensities for missing dilutions
+    # WAVEICA
+    path = '/Users/andreidm/ETH/projects/calibration/data/SRM_SPP_other_methods/waveICA1.csv'
+    normalized_pp = get_data(path, ['P1_PP', 'P2_SPP', 'P2_SRM'], metabolites=pps)
+    normalized_aa = get_data(path, ['P1_AA', 'P2_SAA', 'P2_SRM'], metabolites=aas)
+    X, Y = assemble_dataset(normalized_pp, normalized_aa)
+    print('training for the normalized data')
+    train_baseline_model(X, Y, plot_id='_waceica', save_to=save_to+'baseline/')  # train and test on the entire dataset
+
+    # COMBAT
+    path = '/Users/andreidm/ETH/projects/calibration/data/SRM_SPP_other_methods/combat1.csv'
+    normalized_pp = get_data(path, ['P1_PP', 'P2_SPP', 'P2_SRM'], metabolites=pps)
+    normalized_aa = get_data(path, ['P1_AA', 'P2_SAA', 'P2_SRM'], metabolites=aas)
+    X, Y = assemble_dataset(normalized_pp, normalized_aa)
+    print('training for the normalized data')
+    train_baseline_model(X, Y, plot_id='_combat', save_to=save_to+'baseline/')  # train and test on the entire dataset
+
+    # train_with_missing_dilutions(X,Y, threshold=2e6)  # train to predict intensities for missing dilutions
     # train_with_missing_metabolites(X,Y, metabolite_group='aas', threshold=1.1e6)  # train to predict intensities for missing amino acids
     # train_with_missing_metabolites(X,Y, metabolite_group='pps', threshold=1.1e6)  # train to predict intensities for missing purines / pyrimidines
 
     # TODO:
     #  1. Repeat everything with the best normalization approach
     #  and compare results for waveICA and ComBat (as a sanity check)
-    #  2. Proceed to the idea of using other metabolites as well (not aas / pps)
     #  3. Save particular cases (combinations of dilutions / metabolites) of the best scores
+    #  2. Proceed to the idea of using other metabolites as well (not aas / pps)
+
 
 
 
